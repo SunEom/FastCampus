d24073c4ba2a43d8d5cf3ddcc26c5f5a991c2ac6
SunEom <dkxl0701@gmail.com>
2024-02-28 01:07:38 +0900

    Asset 추가

---
+++ KTV/KTV/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  KTV
+//
+//  Created by 엄태양 on 2/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        button.isUserInteractionEnabled = false
+    }
+
+
+
+}
+
